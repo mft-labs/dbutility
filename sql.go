93db23f535435bd139cea251a11604d56f27e539
adi-bindurani <bindukiranmai.rani@hub4edi.com>
2022-07-21 13:26:30 +0530

    added logger

--- sql.go
+++ sql.go
@@ -2,6 +2,7 @@ package dbutility
 
 import (
 	"database/sql"
+	"amfui/utilities"
 	"fmt"
 	"time"
 )
@@ -20,18 +21,22 @@ const (
 	INSERT_INTO_EVENT_HISTORY = "insert into amf_event_history (event_id,level,message_id,session_id,action_id,text,status,create_time,created_by)"
 )
 
-func (util *DbUtil) DeleteHistory(Db *sql.DB,fromdate,todate,tablename string) error{
+func (util *DbUtil) DeleteHistory(context utilities.AppContext,Db *sql.DB,fromdate,todate,tablename string) error{
 	if fromdate == ""{
 		query := "delete from "+tablename+" where create_time <= $1"
 		resp, err := Db.Exec(query,todate)
 		fmt.Printf("Delete response for %v is: %v\n",tablename,resp)
+		context.Logger.Info("Delete query for %v is: %v\n",tablename,query)
+		context.Logger.Info("Delete response for %v is: %v\n",tablename,resp)
 		if err != nil{
 			return err
 		}
 	} else {
 		query := "delete from "+tablename+" where create_time >= $1 and create_time <= $2"
 		resp, err := Db.Exec(query,fromdate,todate)
+		context.Logger.Info("Delete query for %v is: %v\n",tablename,query)
 		fmt.Printf("Delete response for %v is: %v\n",tablename,resp)
+		context.Logger.Info("Delete response for %v is: %v\n",tablename,resp)
 		if err != nil{
 			return err
 		}
@@ -40,34 +45,42 @@ func (util *DbUtil) DeleteHistory(Db *sql.DB,fromdate,todate,tablename string) e
 	return nil
 }
 
-func (util *DbUtil) InsertToHistoryTable(Db *sql.DB,then time.Time,tablename string) error{
+func (util *DbUtil) InsertToHistoryTable(context utilities.AppContext,Db *sql.DB,then time.Time,tablename string) error{
 	if tablename == "amf_message_history"{
 		Query := INSERT_INTO_MESSAGE_HISTORY+SELECT_MESSAGES+" where create_time <= $1"
+		context.Logger.Info("Insert query for %v is: %v\n",tablename,Query)
 		resp, err := Db.Exec(Query,then.Format("2006-01-02 15:04:05"))
 		fmt.Printf("response for adding data to %v is: %v\n",tablename,resp)
+		context.Logger.Info("response for adding data to %v is: %v\n",tablename,resp)
 		if err != nil{
 			return err
 		}
 	} else if tablename == "amf_session_history"{
 		Query := INSERT_INTO_SESSION_HISTORY+SELECT_SESSIONS+" where create_time <= $1"
+		context.Logger.Info("Insert query for %v is: %v\n",tablename,Query)
 		resp, err := Db.Exec(Query,then.Format("2006-01-02 15:04:05"))
 		fmt.Printf("response for adding data to %v is: %v\n",tablename,resp)
+		context.Logger.Info("response for adding data to %v is: %v\n",tablename,resp)
 		if err != nil{
 			return err
 		}
 		return nil
 	} else if tablename == "amf_session_rel_history"{
 		Query := INSERT_INTO_SESSION_REL_HISTORY+SELECT_SESSION_REL+" where create_time <= $1"
+		context.Logger.Info("Insert query for %v is: %v\n",tablename,Query)
 		resp, err := Db.Exec(Query,then.Format("2006-01-02 15:04:05"))
 		fmt.Printf("response for adding data to %v is: %v\n",tablename,resp)
+		context.Logger.Info("response for adding data to %v is: %v\n",tablename,resp)
 		if err != nil{
 			return err
 		}
 		return nil
 	} else if tablename == "amf_event_history"{
 		Query := INSERT_INTO_EVENT_HISTORY+SELECT_EVENT+" where create_time <= $1"
+		context.Logger.Info("Insert query for %v is: %v\n",tablename,Query)
 		resp, err := Db.Exec(Query,then.Format("2006-01-02 15:04:05"))
 		fmt.Printf("response for adding data to %v is: %v\n",tablename,resp)
+		context.Logger.Info("response for adding data to %v is: %v\n",tablename,resp)
 		if err != nil{
 			return err
 		}
@@ -77,12 +90,14 @@ func (util *DbUtil) InsertToHistoryTable(Db *sql.DB,then time.Time,tablename str
 }
 
 
-func (util *DbUtil) InsertLastMonthHistory(Db *sql.DB,firstOfMonth,lastOfMonth,tablename string) error{
+func (util *DbUtil) InsertLastMonthHistory(context utilities.AppContext,Db *sql.DB,firstOfMonth,lastOfMonth,tablename string) error{
 	if tablename == "amf_message_history"{
 		whereClause := " where create_time >= '"+firstOfMonth+"' and create_time <= '"+lastOfMonth+"'"
 		Query := INSERT_INTO_MESSAGE_HISTORY+SELECT_MESSAGES+whereClause
+		context.Logger.Info("Insert query for %v is: %v\n",tablename,Query)
 		resp, err := Db.Exec(Query)
 		fmt.Printf("response for adding data to %v is: %v\n",tablename,resp)
+		context.Logger.Info("response for adding data to %v is: %v\n",tablename,resp)
 		if err != nil{
 			return err
 		}
@@ -90,8 +105,10 @@ func (util *DbUtil) InsertLastMonthHistory(Db *sql.DB,firstOfMonth,lastOfMonth,t
 	} else if tablename == "amf_session_history"{
 		whereClause := " where create_time >= '"+firstOfMonth+"' and create_time <= '"+lastOfMonth+"'"
 		Query := INSERT_INTO_SESSION_HISTORY+SELECT_SESSIONS+whereClause
+		context.Logger.Info("Insert query for %v is: %v\n",tablename,Query)
 		resp, err := Db.Exec(Query)
 		fmt.Printf("response for adding data to %v is: %v\n",tablename,resp)
+		context.Logger.Info("response for adding data to %v is: %v\n",tablename,resp)
 		if err != nil{
 			return err
 		}
@@ -99,8 +116,10 @@ func (util *DbUtil) InsertLastMonthHistory(Db *sql.DB,firstOfMonth,lastOfMonth,t
 	} else if tablename == "amf_session_rel_history"{
 		whereClause := " where create_time >= '"+firstOfMonth+"' and create_time <= '"+lastOfMonth+"'"
 		Query := INSERT_INTO_SESSION_REL_HISTORY+SELECT_SESSION_REL+whereClause
+		context.Logger.Info("Insert query for %v is: %v\n",tablename,Query)
 		resp, err := Db.Exec(Query)
 		fmt.Printf("response for adding data to %v is: %v\n",tablename,resp)
+		context.Logger.Info("response for adding data to %v is: %v\n",tablename,resp)
 		if err != nil{
 			return err
 		}
@@ -108,8 +127,10 @@ func (util *DbUtil) InsertLastMonthHistory(Db *sql.DB,firstOfMonth,lastOfMonth,t
 	} else if tablename == "amf_event_history"{
 		whereClause := " where create_time >= '"+firstOfMonth+"' and create_time <= '"+lastOfMonth+"'"
 		Query := INSERT_INTO_EVENT_HISTORY+SELECT_EVENT+whereClause
+		context.Logger.Info("Insert query for %v is: %v\n",tablename,Query)
 		resp, err := Db.Exec(Query)
 		fmt.Printf("response for adding data to %v is: %v\n",tablename,resp)
+		context.Logger.Info("response for adding data to %v is: %v\n",tablename,resp)
 		if err != nil{
 			return err
 		}
